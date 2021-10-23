4f0cc8530a6bd2208e6a62d1b1ce8e63cc1e712c
Andreas Meier <andreas.meier@stud.unibas.ch>
2021-10-23 09:37:27 +0200

    Draft schema for commands

--- schema.sql
+++ schema.sql
@@ -1,6 +1,5 @@
 DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS shoppingitem;
-DROP TABLE IF EXISTS shoppinglist;
+DROP TABLE IF EXISTS command;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -9,17 +8,15 @@ CREATE TABLE user (
 );
 
 
-CREATE TABLE shoppingitem (
-  item_key INTEGER PRIMARY KEY AUTOINCREMENT,
-  userid INTEGER NOT NULL,
-  listkey INTEGER NOT NULL,
-  name TEXT NOT NULL,
-  quantity TEXT NOT NULL,
-  shop TEXT
-);
+CREATE TABLE command (
+    command_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    list_id INTEGER NOT NULL,
+    origin TEXT NOT NULL,
+    name TEXT NOT NULL,
+    quantity TEXT NOT NULL,
+    shop NOT NULL,
+    timestamp INTEGER
 
-CREATE TABLE shoppinglist (
-  listkey INTEGER PRIMARY KEY AUTOINCREMENT,
-    userid INTEGER NOT NULL,
-  name TEXT NOT NULL
+
 );
