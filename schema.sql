3a27527476759b0bab35e06c4cffa6e96cd199a1
Andreas Meier <meier89@gmx.ch>
2021-10-31 15:52:03 +0100

    Work on schema

--- schema.sql
+++ schema.sql
@@ -16,7 +16,22 @@ CREATE TABLE command (
     name TEXT NOT NULL,
     quantity TEXT NOT NULL,
     shop NOT NULL,
-    timestamp INTEGER
-
-
+    timestamp INTEGER,
+    foreign key (user_id) REFERENCES user(id),
+    foreign key (list_id) REFERENCES list(list_id)
 );
+
+CREATE TABLE listcommand(
+    command_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT NOT NULL ,
+    list_id NOT NULL,
+    origin TEXT NOT NULL
+);
+
+CREATE TABLE list(
+    list_id INTEGER,
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    PRIMARY KEY (user_id, list_id)
+
+)
