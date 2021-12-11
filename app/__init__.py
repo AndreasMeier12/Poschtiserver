3f5e064dca6d845b1cf8bbb0eb38534c0093a3a1
Andreas Meier <meier89@gmx.ch>
2021-12-11 06:58:18 +0100

    Fix DB initialization and migration

--- app/__init__.py
+++ app/__init__.py
@@ -9,8 +9,8 @@ from config import Config
 
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
--- app/models.py
+++ app/models.py
@@ -6,14 +6,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app import db, login
 
 
-class User():
-    class User(UserMixin, db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        username = db.Column(db.String(64), index=True, unique=True)
-        email = db.Column(db.String(120), index=True, unique=True)
-        password_hash = db.Column(db.String(128))
-        list_commands = db.relationship('ListCommand', backref='author', lazy='dynamic')
-        item_commands = db.relationship('ItemCommand', backref='author', lazy='dynamic')
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -31,16 +28,5 @@ def load_user(id):
 
 
 
-
-class ListCommand(db.Model):
-    command_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, ForeignKey('User.id'),  nullable=False, primary_key=True)
-    list_id = db.Column(db.Integer, nullable=False)
-
-
-class ItemCommand(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, ForeignKey('User.id'),  nullable=False)
-    list_id = db.Column(db.Integer)
 
 
