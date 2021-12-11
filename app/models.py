5a97ef38e36b5b939738ef5dd7cb246cb815dd5e
Andreas Meier <meier89@gmx.ch>
2021-12-11 14:06:45 +0100

    Add list commands to db

--- app/models.py
+++ app/models.py
@@ -10,6 +10,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    list_commands = db.relationship('ListCommand', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -28,4 +29,11 @@ def load_user(id):
 
 
 
+
+
+class ListCommand(db.Model):
+    command_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey('user.id'),  nullable=False, primary_key=True)
+    list_id = db.Column(db.Integer, nullable=False)
+    type = db.Column(db.Integer, nullable=False)
 
